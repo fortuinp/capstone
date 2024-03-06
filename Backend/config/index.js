b54a115f0e2281104868886f8eb037dfd18b5ffb
fortuinp <pinda.fortuin@gmail.com>
2024-03-06 10:10:13 +0200

    configContent

--- Backend/config/index.js
+++ Backend/config/index.js
@@ -1,1 +1,14 @@
-//index
+import { createPool } from "mysql";
+import { config } from "dotenv";
+config()
+let dbconnection=createPool({
+    host : process.env.DB_HOST,
+    database: process.env.DB_Name,
+    user: process.env.DB_Username,
+    password: process.env.DB_UserPass,
+    multipleStatements:true,
+    connectionLimit:30
+})
+export{
+    dbconnection
+}
