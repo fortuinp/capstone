2bc1e3aae75a7e49f919f25cb9efe390277296c0
fortuinp <pinda.fortuin@gmail.com>
2024-03-15 13:16:12 +0200

    admin

--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -6,6 +6,7 @@
           <li class="nav-item mx-2">
             <a class="nav-link link-light" aria-current="page" href="#">Products</a>
           </li>
+          
           <li class="nav-item mx-4">
             <router-link to="/users" class="nav-link link-light">Users</router-link>
           </li>
@@ -23,27 +24,27 @@
           <button class=" btn btn-dark"> Filter</button>
         </div>
         <div class="col-2 mx-3">
-          <button class=" btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal"> Add a User</button>
+          <button class=" btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal"> Add Product</button>
         </div>
         <!-- Modal-->
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Add User</h1>
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Add Product</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body">
                 <h5 class="text-start">Name: </h5>
-                <input v-model="payload.prodName" type="text" placeholder="Name" class="form-control">
-                <h5 class="text-start mt-2">quantity: </h5>
+                <input v-model="payload.prodName" type="text" placeholder="name" class="form-control">
+                <h5 class="text-start mt-2">Quantity: </h5>
                 <input v-model="payload.quantity" type="text mt-2" placeholder="quantity" class="form-control">
-                <h5 class="text-start mt-2">amount: </h5>
-                <input v-model="payload.amount" type="text" placeholder="amt" class="form-control">
-                <h5 class="text-start mt-2">cat: </h5>
-                <input v-model="payload.prodCategory" type="text" placeholder="cat" class="form-control">
-                <h5 class="text-start mt-2">des: </h5>
-                <input v-model="payload.prodDescription" type="text" placeholder="des" class="form-control">
+                <h5 class="text-start mt-2">Amount: </h5>
+                <input v-model="payload.amount" type="text" placeholder="amount" class="form-control">
+                <h5 class="text-start mt-2">Category: </h5>
+                <input v-model="payload.prodCategory" type="text" placeholder="category" class="form-control">
+                <h5 class="text-start mt-2">Description: </h5>
+                <input v-model="payload.prodDescription" type="text" placeholder="description" class="form-control">
                 <h5 class="text-start mt-2">url: </h5>
                 <input v-model="payload.prodUrl" type="text" placeholder="url" class="form-control">
                 <h5 class="text-start mt-2">url2: </h5>
@@ -52,42 +53,11 @@
               </div>
               <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button @click="addProd()" type="button" class="btn btn-dark">Add pro</button>
+                <button @click="addProduct()" type="button" class="btn btn-dark">Add product</button>
               </div>
             </div>
           </div>
         </div>
-        <!-- Modal edit
-        <div class="modal fade" :id="'edit' + product.prodIDrodID" tabindex="-1" aria-labelledby="edit" aria-hidden="true"   v-for="product in products" :key="product.prodID">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h1 class="modal-title fs-5" id="edit">Edit User</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                <h5 class="text-start">Name: </h5>
-                <input v-model="payload.prodName" type="text" :placeholder="product.prodName" class="form-control">
-                <h5 class="text-start mt-2">quantity: </h5>
-                <input v-model="payload.quantity" type="text" :placeholder="product.quantity" class="form-control">
-                <h5 class="text-start mt-2">amt </h5>
-                <input v-model="payload.amount" type="text" :placeholder="product.amount" class="form-control">
-                <h5 class="text-start mt-2">cat: </h5>
-                <input v-model="payload.prodCategory" type="text" :placeholder="product.prodCategory" class="form-control">
-                <h5 class="text-start mt-2">des: </h5>
-                <input v-model="payload.prodDescription" type="text" :placeholder="product.prodDescription" class="form-control">
-                <h5 class="text-start mt-2">url: </h5>
-                <input v-model="payload.prodUrl" type="text" :placeholder="product.prodUrl" class="form-control">
-                <h5 class="text-start mt-2">url2: </h5>
-                <input v-model="payload.prodUrl2" type="text" :placeholder="product.prodUrl2" class="form-control">
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button  @click="editProd(product.ProdID)" type="button" class="btn btn-dark">Update User</button>
-              </div>
-            </div>
-          </div>
-        </div> -->
 
         <!-- Modal edit -->
 <div class="modal fade" :id="'edit' + product.prodID" tabindex="-1" aria-labelledby="edit" aria-hidden="true" v-for="product in products" :key="product.prodID">
@@ -100,13 +70,13 @@
       <div class="modal-body">
         <h5 class="text-start">Name: </h5>
         <input v-model="payload.prodName" type="text" class="form-control">
-        <h5 class="text-start mt-2">quantity: </h5>
+        <h5 class="text-start mt-2">Quantity: </h5>
         <input v-model="payload.quantity" type="text" class="form-control">
-        <h5 class="text-start mt-2">amt </h5>
+        <h5 class="text-start mt-2">Amount </h5>
         <input v-model="payload.amount" type="text" class="form-control">
-        <h5 class="text-start mt-2">cat: </h5>
+        <h5 class="text-start mt-2">Category: </h5>
         <input v-model="payload.prodCategory" type="text" class="form-control">
-        <h5 class="text-start mt-2">des: </h5>
+        <h5 class="text-start mt-2">Description: </h5>
         <input v-model="payload.prodDescription" type="text" class="form-control">
         <h5 class="text-start mt-2">url: </h5>
         <input v-model="payload.prodUrl" type="text" class="form-control">
@@ -115,7 +85,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button @click="editProd(product.prodID)" type="button" class="btn btn-dark">Update User</button>
+        <button @click="editProduct(product.prodID)" type="button" class="btn btn-dark">Update User</button>
       </div>
     </div>
   </div>
@@ -135,7 +105,7 @@
               </div>
               <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button @click.prevent="deleteProduct(product.prodID)" type="button" class="btn btn-dark">Delete prod</button>
+                <button @click.prevent="deleteProduct(product.prodID)" type="button" class="btn btn-dark">Delete product</button>
               </div>
             </div>
           </div>
@@ -145,10 +115,10 @@
     <table class="AdiCont table mt-5 text-center mx-auto" v-if="products">
       <thead>
         <tr>
-          <th>  ID</th>
-          <th> Name</th>
-          <th>q</th>
-          <th>a</th>
+          <th>Product ID</th>
+          <th>Product Name</th>
+          <th>Quantity</th>
+          <th>Amount</th>
           
         </tr>
       </thead>
@@ -218,10 +188,10 @@
       deleteProduct(prodID) {
         this.$store.dispatch('deleteProduct', prodID)
       },
-      addProd() {
+      addProduct() {
         this.$store.dispatch('addProduct', this.payload)
       },
-      editProd(prodID) {
+      editProduct(prodID) {
         const updateData = Object.assign({}, {prodID}, this.payload)
         this.$store.dispatch('updateProduct', updateData)
       }
